8b95625af22b38f1a7a3b6eebdf1155f3722988a
Igor Doronin <ihardaronin@gmail.com>
2024-06-27 14:41:55 +0300

    add csv file

--- svetparse/svetparse/spiders/svetnewparse.py
+++ svetparse/svetparse/spiders/svetnewparse.py
@@ -1,6 +1,5 @@
 import scrapy
 
-
 class SvetnewparseSpider(scrapy.Spider):
     name = "svetnewparse"
     allowed_domains = ["https://ami.by"]
@@ -11,12 +10,29 @@ class SvetnewparseSpider(scrapy.Spider):
                   "https://ami.by/catalog/gardenlamps.html"
                   ]
 
+
+
     def parse(self, response):
-        lamps = response.css('div.item_new')
-        for lamp in lamps:
-            yield {
-                'name': lamp.css('div.itemtitle a::text').get(),
-                'price': lamp.css('div.Price span.digitsGroup2::text').get() + '.' +
-                         lamp.css('div.Price span.digitsGroup3::text').get(),
-                'url': lamp.css('div.itemtitle a').attrib['href']
-            }
+
+        try:
+            lamps = response.css('div.item_new')
+            for lamp in lamps:
+                name = lamp.css('div.itemtitle a::text').get()
+                price = lamp.css('div.Price span.digitsGroup2::text').get() + '.' + lamp.css(
+                    'div.Price span.digitsGroup3::text').get()
+                url = lamp.css('div.itemtitle a').attrib['href']
+
+                yield {
+                    'name': name,
+                    'price': price,
+                    'url': url
+                }
+
+        except:
+            print('Произошла ошибка при парсинге')
+
+
+
+
+
+
